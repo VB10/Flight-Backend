6c8c6d5c97364ddbf2f4f2cf09df30390c354c0b
Veli Bacik <vbacik.10@gmail.com>
2022-05-03 19:14:43 +0300

    Update open_sky_controller.js

--- functions/controllers/open_sky_controller.js
+++ functions/controllers/open_sky_controller.js
@@ -2,7 +2,7 @@ var request = require("request");
 var stateResponse = require("../util/constant");
 
 const url = 
-  "https://vb10:opensky**@opensky-network.org/api/states/all?lamin=35.9025&lomin=25.90902&lamax=42.02683&lomax=44.5742";
+  "https://user:pw@opensky-network.org/api/states/all?lamin=35.9025&lomin=25.90902&lamax=42.02683&lomax=44.5742";
 const flightStates = onComplete => {
   request(url, function(error, response, body) {
     console.log("error:", error); // Print the error if one occurred
